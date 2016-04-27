3d3ade363ffae89d70ab3e581bc2f6cfaa76de0c
nguyenquangsang <nguyenquangsang2810@gmail.com>
2016-04-27 20:22:36 +0700

    Added files via upload

--- untitled/script.js
+++ untitled/script.js
@@ -1,3 +1,12 @@
 /**
  * Created by NguyenQuangSang on 26/04/2016.
  */
+
+
+var app = angular.module('myCV', []);
+app.controller('Controller', function($scope, $http) {
+    $http.get("profile.json")
+        .then(function(response) {
+            $scope.myData = response.data.profile;
+        });
+});
